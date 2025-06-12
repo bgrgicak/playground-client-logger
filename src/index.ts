818f16f0a38d1fe244f0927eb91590c91ccb7358
Bero <berislav.grgicak@gmail.com>
2025-06-12 09:05:39 +0200

    Collect PHP errors with the Playground logger

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import { startPlaygroundWeb } from '@wp-playground/client';
+import { logger, addCrashListener, collectPhpLogs } from '@php-wasm/logger';
+
+const iframe = document.getElementById('wp') as HTMLIFrameElement;
+const client = await startPlaygroundWeb({
+  iframe,
+  remoteUrl: 'https://playground.wordpress.net/remote.html',
+  blueprint: {
+    "steps": [
+      {
+        "step": "writeFile",
+        "path": "/wordpress/wp-content/plugins/fail.php",
+        "data": {
+          "resource": "url",
+          "url": "https://gist.githubusercontent.com/bgrgicak/326aa07eb7bb58edc1c972f566c867a3/raw/3e543fbd150acec0cb58038a7931e24cbd5ccdd3/fail.php"
+        }
+      },
+    ]
+  }
+});
+
+collectPhpLogs( logger, client );
+
+addCrashListener( logger, ( event: any ) => {
+  console.log(event.detail);
+} );
+
+
+await client.isReady();
+
+// Now trigger a fatal error by activating the fail.php plugin.
+await client.run(
+  {
+    code: `
+      <?php
+      require_once '/wordpress/wp-load.php' ;
+      activate_plugin( 'fail.php' );
+      `
+  }
+);
